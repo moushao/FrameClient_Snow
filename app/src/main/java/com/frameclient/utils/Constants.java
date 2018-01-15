bab53512e4bab0d5e29261da06e0f5ec576f10a1
MouShao <moushao1990@gmail.com>
2018-01-15 10:18:25 +0800

    laibing project

---
+++ app/src/main/java/com/frameclient/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.frameclient.utils;
+
+/**
+ * Created by Zhang on 2017/12/20.
+ */
+
+public class Constants {
+
+        public static  String IP_ADDRESS ="118.112.185.226:8898";
+//        public static  String IP_ADDRESS ="118.112.185.226:8805";
+//    public static String IP_ADDRESS = "192.168.0.104:6611";
+    public static final String LOGIN_NAME = "sa";
+    public static final String LOGIN_PWD = "sa";
+
+    public static String BASE_URL = "http://192.168.0.104:8081/";
+
+}
